79d597eee8f567ce04420f5dca5db2d8771863e2
luke <lukevance90@gmail.com>
2019-10-24 12:24:45 -0600

    added weekly scores data in effects and manipulation for table

--- src/WeeklyScores/index.js
+++ src/WeeklyScores/index.js
@@ -9,10 +9,37 @@ import Tooltip from '@material-ui/core/Tooltip';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Paper from '@material-ui/core/Paper';
 
+const range = (start, end) => {
+    let range = [];
+    for (let i = start; i <= end; i++){
+        range.push(i);
+    }
+    return range;
+};
+
 function WeeklyScoresTable(props) {
     const [weekSummaries, setWeekSummaries] = useState([]);
 
+    useEffect(() => {
+        fetch(`https://8fqfwnzfyb.execute-api.us-east-1.amazonaws.com/dev/leagues/${props.leagueId}`)
+            .then(response => response.json())
+            .then(data => {
+                // pull out active week and ensure it's a number
+                const currentWeek = data.status.currentMatchupPeriod * 1;
+                // create array of weeks from current week
+                const weeksArray = range(1, currentWeek - 1).reverse();
+                const weeksData = weeksArray.map(wk => {
+                    return {
+                        matchupPeriodId: wk,
+                        matchups: data.schedule.filter(matchup => matchup.matchupPeriodId == wk)
+                    }
+                });
+                setWeekSummaries(weeksData); // set weekSummaries in state
+            });
+    }, [props.leagueId]);
+
     const columns = ["Week", "Team", "Score", "Rank", "Outcome"];
+    console.log(weekSummaries);
     return (
         <Paper>
             <Table>
@@ -28,19 +55,26 @@ function WeeklyScoresTable(props) {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {weekSummaries.map(week => {
+                    {weekSummaries.length > 0 ? weekSummaries.map(week => {
+                        const winners = week.matchups.map(matchup => {
+                            console.log(matchup.winner);
+                            return matchup[matchup.winner.toLowerCase()]
+                        });
+                        const highScore = winners.sort((a, b) => (a.totalPoints > b.totalPoints) ? -1 : 1)[0];
+                        console.log(winners);
                         return (
-                            <TableRow>
+                            <TableRow key={week.matchupPeriodId}>
                                 <TableCell>{week.matchupPeriodId}</TableCell>
                                 {/* TODO: add func for finding week high score */}
-                                <TableCell>{week.winner.toLowerCase()}</TableCell>
+                                <TableCell>{highScore.teamId}</TableCell>
+                                <TableCell>{highScore.totalPoints}</TableCell>
                             </TableRow>
                         );
-                    })}
+                    }): null}
                 </TableBody>
             </Table>
         </Paper>
-    )
-}
+    );
+};
 
 export default WeeklyScoresTable;
