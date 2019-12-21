ef5ccc94a18e4ec3776e09d9fc712f9b587617f7
luke <lukevance90@gmail.com>
2019-12-20 20:22:05 -0700

    fixed bug in util

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,9 @@
+const sum = (items, prop) => {
+    return items.reduce((a, b) => {
+        return a + b[prop];
+    }, 0);
+};
+
 module.exports.totalPointsForPosition = (players, position) => {
     const positionPlayers = players.filter(plyr => plyr.position === position && plyr.starter === true);
     const totalPoints = Math.round(sum(positionPlayers, "points") * 10) / 10;
@@ -13,11 +19,7 @@ module.exports.teamNameFromId = (id, teams) => {
     }
 };
 
-module.exports.sum = (items, prop) => {
-    return items.reduce((a, b) => {
-        return a + b[prop];
-    }, 0);
-};
+module.exports.sum = sum;
 
 module.exports.range = (start, end) => {
     let range = [];
